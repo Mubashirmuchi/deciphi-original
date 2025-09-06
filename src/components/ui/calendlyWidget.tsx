908e45936d028debd338d55e0c6233294572d53e
Mubashir <mubashirmuchi75@gmail.com>
2025-09-06 20:32:58 +0530

    responsive bok a call

--- src/components/ui/calendlyWidget.tsx
+++ src/components/ui/calendlyWidget.tsx
@@ -4,13 +4,17 @@ import { InlineWidget } from "react-calendly";
 
 const CalendlyWidget = () => {
   return (
-    <div className="h-fit">
+    <div className="h-full w-full ">
       <InlineWidget
         url="https://calendly.com/mubashirfreston/30min"
-        styles={{ height: "700px", minWidth: "320px" }}
+        styles={{
+          height: "800px",
+          minHeight: "800px",
+          minWidth: "320px",
+          width: "100%",
+        }}
       />
     </div>
   );
 };
-
 export default memo(CalendlyWidget);
